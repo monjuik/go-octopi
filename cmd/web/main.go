aec53d7e2ad2d10dbc138764ebe6c0c7815408ac
Alexey Malinin <amalinin@MacBook-Air-Alexey-2.local>
2025-11-22 18:51:04 +0200

    Introduce server port in env variable

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,15 +4,25 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 
 	gooctopi "github.com/monjuik/go-octopi"
 )
+
+const serverPortEnv = "OCTOPI_SERVER_PORT"
 
 func main() {
 	logger := gooctopi.NewLogger("web")
 	handler := gooctopi.NewServer()
 
 	port := 8080
+	if envPort := os.Getenv(serverPortEnv); envPort != "" {
+		if parsed, err := strconv.Atoi(envPort); err == nil {
+			port = parsed
+		} else {
+			logger.Printf("invalid %s %q, using default %d: %v", serverPortEnv, envPort, port, err)
+		}
+	}
 	addr := fmt.Sprintf(":%d", port)
 
 	logger.Printf("Listening at http://localhost:%d", port)
